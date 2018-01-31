cd98dd9d5d9215debf38eb034d7915e4b2195d73
Electromaster232 <25728719+Electromaster232@users.noreply.github.com>
2018-01-31 08:57:27 -0500

    Create games.js

---
+++ games.js
@@ -0,0 +1,16 @@
+function why() {
+    			if (confirm('Ready to play?')) {
+    			// Save it!
+    			window.open("https://www.w3schools.com");
+			} else {
+    			// Do nothing!
+			}
+}
+function bird() {
+if (confirm('Ready to play?')) {
+    			// Save it!
+    			window.open("https://bsodstudios.github.io/bsod-bird");
+			} else {
+    			// Do nothing!
+			}
+}
